42314650da55a059a6e89eac17c88623cf4b29e0
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 02:32:52 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   # your code here
-  # result = []
-  # array.each do |element|
-  #     result << element ** 2
-  # end
-  # return result
-  return array.collect { |element| element ** 2 }
+  result = []
+  array.each do |element|
+      result << element ** 2
+  end
+  return result
 end
